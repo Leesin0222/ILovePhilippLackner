614793f200e350e1641bc7b0ff23e2f0d57935cc
Leesin0222 <dragon02229@naver.com>
2022-01-13 03:28:50 +0900

    📑 :: shoppingitems Entity 생성

--- MVVMShoppingListApp/app/src/main/java/com/yongjincompany/mvvmshoppinglistapp/ShoppingItem.kt
+++ MVVMShoppingListApp/app/src/main/java/com/yongjincompany/mvvmshoppinglistapp/ShoppingItem.kt
@@ -1,3 +1,16 @@
 package com.yongjincompany.mvvmshoppinglistapp
 
-data class ShoppingItem()
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shopping_items")
+data class ShoppingItem(
+    @ColumnInfo(name = "item_name")
+    var name: String,
+    @ColumnInfo(name = "item_amount")
+    var amount: Int
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int ? = null
+}
