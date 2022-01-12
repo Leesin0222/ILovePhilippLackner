0509a5f81302bf536b2a7f685819ab149eb31fbb
Leesin0222 <dragon02229@naver.com>
2022-01-13 04:29:30 +0900

    📑 :: ui.shoppinglist로 파일 구조 변경

--- MVVMShoppingListApp/app/src/main/java/com/yongjincompany/mvvmshoppinglistapp/ui/shoppinglist/ShoppingActivity.kt
+++ MVVMShoppingListApp/app/src/main/java/com/yongjincompany/mvvmshoppinglistapp/ui/shoppinglist/ShoppingActivity.kt
@@ -1,11 +1,16 @@
-package com.yongjincompany.mvvmshoppinglistapp
+package com.yongjincompany.mvvmshoppinglistapp.ui.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
+import com.yongjincompany.mvvmshoppinglistapp.R
 
 class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
+
+        val viewModel = ViewModelProviders.of(this).get(ShoppingViewModel::class.java)
+
     }
 }
