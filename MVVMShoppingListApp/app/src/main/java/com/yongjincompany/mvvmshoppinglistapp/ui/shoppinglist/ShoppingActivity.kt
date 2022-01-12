71392d1337a3e8cd3b2e0dff96c4934f352733f6
Leesin0222 <dragon02229@naver.com>
2022-01-13 04:36:23 +0900

    📑 :: 잇고잇고이은 val

--- MVVMShoppingListApp/app/src/main/java/com/yongjincompany/mvvmshoppinglistapp/ui/shoppinglist/ShoppingActivity.kt
+++ MVVMShoppingListApp/app/src/main/java/com/yongjincompany/mvvmshoppinglistapp/ui/shoppinglist/ShoppingActivity.kt
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProviders
 import com.yongjincompany.mvvmshoppinglistapp.R
+import com.yongjincompany.mvvmshoppinglistapp.data.db.ShoppingDatabase
+import com.yongjincompany.mvvmshoppinglistapp.data.repositories.ShoppingRepository
 
 class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
+
+        val database = ShoppingDatabase(this)
+        val repository = ShoppingRepository(database)
+        val factory = ShoppingViewModelFactory(repository)
 
         val viewModel = ViewModelProviders.of(this).get(ShoppingViewModel::class.java)
 
