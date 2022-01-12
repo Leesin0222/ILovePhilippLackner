c7bef5c3250dcc7209a44a1cf737f7ae537217b3
Leesin0222 <dragon02229@naver.com>
2022-01-13 03:56:23 +0900

    📑 :: 파일 구조 변경

--- MVVMShoppingListApp/app/src/main/java/com/yongjincompany/mvvmshoppinglistapp/data/db/ShoppingDatabase.kt
+++ MVVMShoppingListApp/app/src/main/java/com/yongjincompany/mvvmshoppinglistapp/data/db/ShoppingDatabase.kt
@@ -1,9 +1,10 @@
-package com.yongjincompany.mvvmshoppinglistapp
+package com.yongjincompany.mvvmshoppinglistapp.data.db
 
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.yongjincompany.mvvmshoppinglistapp.data.db.entities.ShoppingItem
 
 @Database(
     entities = [ShoppingItem::class],
