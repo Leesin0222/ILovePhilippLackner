d96fda6d9b6de951fec4b277dc4077f0433256ae
Leesin0222 <dragon02229@naver.com>
2022-01-13 04:11:15 +0900

    📑 :: ShoppingRepository 완

--- MVVMShoppingListApp/app/src/main/java/com/yongjincompany/mvvmshoppinglistapp/data/repositories/ShoppingRepository.kt
+++ MVVMShoppingListApp/app/src/main/java/com/yongjincompany/mvvmshoppinglistapp/data/repositories/ShoppingRepository.kt
@@ -1,4 +1,14 @@
 package com.yongjincompany.mvvmshoppinglistapp.data.repositories
 
-class ShoppingRepository {
+import com.yongjincompany.mvvmshoppinglistapp.data.db.ShoppingDatabase
+import com.yongjincompany.mvvmshoppinglistapp.data.db.entities.ShoppingItem
+
+class ShoppingRepository(
+    private val db: ShoppingDatabase
+) {
+    suspend fun upsert(item: ShoppingItem) = db.getShoppingDao().upsert(item)
+
+    suspend fun delete(item: ShoppingItem) = db.getShoppingDao().delete(item)
+
+    fun getAllShoppingItems() = db.getShoppingDao().getAllShoppingItems()
 }
