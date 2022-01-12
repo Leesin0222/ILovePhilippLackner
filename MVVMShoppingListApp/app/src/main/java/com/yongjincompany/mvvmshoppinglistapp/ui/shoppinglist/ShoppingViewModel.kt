256abe42caf4f4927abc81a83a1aea49ba75369b
Leesin0222 <dragon02229@naver.com>
2022-01-13 04:28:52 +0900

    📑 :: Shoppingviewmodel repository 오ㅓㅏ연결

--- MVVMShoppingListApp/app/src/main/java/com/yongjincompany/mvvmshoppinglistapp/ui/shoppinglist/ShoppingViewModel.kt
+++ MVVMShoppingListApp/app/src/main/java/com/yongjincompany/mvvmshoppinglistapp/ui/shoppinglist/ShoppingViewModel.kt
@@ -1,4 +1,24 @@
 package com.yongjincompany.mvvmshoppinglistapp.ui.shoppinglist
 
-class ShoppingViewModel {
+import androidx.lifecycle.ViewModel
+import com.yongjincompany.mvvmshoppinglistapp.data.db.ShoppingDatabase
+import com.yongjincompany.mvvmshoppinglistapp.data.db.entities.ShoppingItem
+import com.yongjincompany.mvvmshoppinglistapp.data.repositories.ShoppingRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ShoppingViewModel(private val repository: ShoppingRepository): ViewModel() {
+
+    fun upsert(item: ShoppingItem) = CoroutineScope(Dispatchers.Main).launch {
+        repository.upsert(item)
+    }
+
+    fun delete(item: ShoppingItem) = CoroutineScope(Dispatchers.Main).launch {
+        repository.delete(item)
+    }
+
+    fun getAllShoppingItems() = repository.getAllShoppingItems()
+
+
 }
