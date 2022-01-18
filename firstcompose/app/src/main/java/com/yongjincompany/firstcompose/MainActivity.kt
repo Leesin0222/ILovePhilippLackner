58ca6f995e2d3efec1afa94dfa00651e01a4e3f0
Leesin0222 <dragon02229@naver.com>
2022-01-19 02:27:11 +0900

    📑 :: 1강 내용 끝!

--- firstcompose/app/src/main/java/com/yongjincompany/firstcompose/MainActivity.kt
+++ firstcompose/app/src/main/java/com/yongjincompany/firstcompose/MainActivity.kt
@@ -14,12 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FirstcomposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            Greeting("Yongjin")
         }
     }
 }
@@ -32,7 +27,5 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    FirstcomposeTheme {
-        Greeting("Android")
-    }
+    Greeting("kid")
 }
