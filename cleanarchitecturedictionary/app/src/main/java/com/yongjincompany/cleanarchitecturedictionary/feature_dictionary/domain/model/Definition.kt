86b2113d976102c5d5448cf2434294e7cd915083
Leesin0222 <dragon02229@naver.com>
2022-01-21 09:16:06 +0900

    ➕ :: domain - model 에 추가

---
+++ cleanarchitecturedictionary/app/src/main/java/com/yongjincompany/cleanarchitecturedictionary/feature_dictionary/domain/model/Definition.kt
@@ -0,0 +1,7 @@
+package com.yongjincompany.cleanarchitecturedictionary.feature_dictionary.domain.model
+
+data class Definition(
+    val antonyms: List<String>,
+    val definition: String,
+    val synonyms: List<String>
+)
