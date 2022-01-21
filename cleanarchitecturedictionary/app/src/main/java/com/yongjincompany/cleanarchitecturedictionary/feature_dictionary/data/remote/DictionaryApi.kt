e6d09e863cd2397022dac47158c3e66794a450d9
Leesin0222 <dragon02229@naver.com>
2022-01-21 09:07:42 +0900

    📑 :: GET 메소드 완

--- cleanarchitecturedictionary/app/src/main/java/com/yongjincompany/cleanarchitecturedictionary/feature_dictionary/data/remote/DictionaryApi.kt
+++ cleanarchitecturedictionary/app/src/main/java/com/yongjincompany/cleanarchitecturedictionary/feature_dictionary/data/remote/DictionaryApi.kt
@@ -1,4 +1,13 @@
 package com.yongjincompany.cleanarchitecturedictionary.feature_dictionary.data.remote
 
+import com.yongjincompany.cleanarchitecturedictionary.feature_dictionary.data.remote.dto.WordInfoDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface DictionaryApi {
+
+    @GET("/api/v2/entries/ko/{word}")
+    suspend fun getWordInfo(
+        @Path("word") word: String
+    ): List<WordInfoDto>
 }
