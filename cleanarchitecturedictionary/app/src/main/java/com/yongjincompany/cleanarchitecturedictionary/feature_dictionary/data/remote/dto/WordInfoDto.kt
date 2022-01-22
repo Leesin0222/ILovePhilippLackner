5f0f8effb8b5806e4a8fd931ad2e2115fdd585a9
Leesin0222 <dragon02229@naver.com>
2022-01-22 14:27:19 +0900

    🚬 :: Dto 삭제

--- cleanarchitecturedictionary/app/src/main/java/com/yongjincompany/cleanarchitecturedictionary/feature_dictionary/data/remote/dto/WordInfoDto.kt
+++
@@ -1,7 +0,0 @@
-package com.yongjincompany.cleanarchitecturedictionary.feature_dictionary.data.remote.dto
-
-data class WordInfoDto(
-    val meanings: List<MeaningDto>,
-    val phonetics: List<PhoneticDto>,
-    val word: String
-)
